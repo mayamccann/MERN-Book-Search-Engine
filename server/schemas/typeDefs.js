51a2dda763af2f7dcd6af1d13a313e032e74ae06
Maya McCann <mccannmaya@gmail.com>
2023-11-16 20:00:42 -0500

    const typeDefs for type for typeDefs.js for schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,39 @@
+// similar to module project
 const {} = require('apollo-server-express');
+const { User } = require('../models');
 
-const typeDefs = gql'
-type: Query {}
+const typeDefs = '
+type Query {
+    _id: ID!
+    name: String!
+}
 
+type Matchup {
+    _id: ID!
+    : String!
+    : String!
+    : Int
+
+}
 ';
+
+type {
+    token: ID!
+    user: User
+}
+
+module.exports = typeDefs;
+
+
+  type Query {
+    tech: [Tech]
+    matchups(_id: String): [Matchup]
+  }
+
+  type Mutation {
+    createMatchup(tech1: String!, tech2: String!): Matchup
+    createVote(_id: String!, techNum: Int!): Matchup
+  }
+`;
 
 module.exports = typeDefs;
