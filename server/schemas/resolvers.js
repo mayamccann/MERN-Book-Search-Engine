2eb1275c5f27f53d54023aeeafa4750cf9e81443
Maya McCann <mccannmaya@gmail.com>
2023-11-16 19:53:40 -0500

    query code demo for resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,17 @@
+// similar to module project
+
 const { User } = require('../models');
 
 const resolvers = {
     Query: {
-
-    }
+        tech: async () => {
+            return Tech.find({});
+          },
+          matchups: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return Matchup.find(params);
+          },
+    },
 }
 
 module.exports = resolvers;
